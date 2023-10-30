94b0ae449d97fb4ea156761350b43fe12722cc1f
“elif-cotton” <elif.watercolors@gmail.com>
2023-10-30 19:17:10 -0300

    final

--- EjemploGit/src/ejemplogit/EjemploGit.java
+++ EjemploGit/src/ejemplogit/EjemploGit.java
@@ -1,6 +1,8 @@
 
 package ejemplogit;
 
+import Entidades.Gato;
+
 /**
  *
  * @author javer
@@ -11,7 +13,15 @@ public class EjemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Gato miGato = new Gato("Whiskers", 3, 4.5, "Mediano");
+
+        System.out.println("Nombre del gato: " + miGato.getNombre());
+        System.out.println("Edad del gato: " + miGato.getEdad());
+        System.out.println("Peso del gato: " + miGato.getPeso());
+        System.out.println("Tamaño del gato: " + miGato.getTamaño());
+
+        miGato.maullar();
+        miGato.comer();
     }
     
 }
