9c8da3e3c7a9be0a71fe00dd9eeeca0637e317e9
“elif-cotton” <elif.watercolors@gmail.com>
2023-10-30 19:12:25 -0300

    actualizacion2

--- EjemploGit/src/Entidades/Gato.java
+++ EjemploGit/src/Entidades/Gato.java
@@ -11,4 +11,53 @@ public class Gato {
     private int edad;     // Atributo privado
     private double peso;  // Atributo privado
     private String tamaño; // Atributo privado
+
+    public Gato() {
+    }
+
+    public Gato(String nombre, int edad, double peso, String tamaño) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.tamaño = tamaño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" + "nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + ", tama\u00f1o=" + tamaño + '}';
+    }
+    
+    
 }
